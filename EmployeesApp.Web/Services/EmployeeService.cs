8f60a96c29283469ccb93a9cbe1bb5a639978463
Nowaxial <Nowaxial@users.noreply.github.com>
2025-05-15 12:05:04 +0200

    Add EmployeeService with employee management methods
    Introduced the EmployeeService class in EmployeeService.cs,
    including a private list of sample Employee objects.
    Implemented methods to add an employee, retrieve an employee by ID,
    and get all employees sorted by name. Added a using directive
    for EmployeesApp.Web.Models.
    
    Co-authored-by: E-Rx <119680976+E-Rx@users.noreply.github.com>
    Co-authored-by: Zhodes <106345228+Zhodes@users.noreply.github.com>

--- EmployeesApp.Web/Services/EmployeeService.cs
+++ EmployeesApp.Web/Services/EmployeeService.cs
@@ -1,6 +1,23 @@
-﻿namespace EmployeesApp.Web.Services
+﻿using EmployeesApp.Web.Models;
+namespace EmployeesApp.Web.Services
 {
     public class EmployeeService
     {
+        List<Employee> employees =
+            [
+             new Employee { Id = 54, Name = "Olle Jonsson", Email = "olle.jonsson@jonsson.it"},
+             new Employee { Id = 100, Name = "Zakk Elfving", Email = "Zakk.Elfving@gmail.com"},
+             new Employee { Id = 234, Name = "Caroline Durant", Email = "caro@durant.com"},
+            ];
+
+        public void AddEmployee(Employee employee)
+        {
+            employee.Id = employees.Count == 0 ? 1 : employees.Max(e => e.Id) + 1;
+            employees.Add(employee);
+        }
+
+        public void GetEmployeeById(int id) => employees.Single(e => e.Id == id);
+
+        public void GetAllEmployees() => employees.OrderBy(e => e.Name).ToArray();
     }
 }
